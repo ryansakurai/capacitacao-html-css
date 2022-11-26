ec1873d7f18883563c6d8a50283abc059c67e4bf
Ryan Sakurai <ryansakurai03@gmail.com>
2022-11-26 19:40:42 -0300

    js consertado

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndice = 1;
+mostrarSlide(slideIndice);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// pelas setas
+function avancarSlide(n) {
+    mostrarSlide(slideIndice += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+// pelas bolinhas
+function esteSlide(n) {
+    mostrarSlide(slideIndice = n);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  let dots = document.getElementsByClassName("bolinhas");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function mostrarSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let bolinhas = document.getElementsByClassName("bolinha");
+    if (n > slides.length){
+        slideIndice = 1
+    }
+    else if (n < 1){
+        slideIndice = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < bolinhas.length; i++) {
+        bolinhas[i].className = bolinhas[i].className.replace(" active", "");
+    }
+
+    slides[slideIndice-1].style.display = "block";
+    bolinhas[slideIndice-1].className += " active";
 }
